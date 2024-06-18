ee8a7783acff6c67f9479e82fd6cdecd56daa6a2
Urmila-Chaudhary <161577372+Urmila-Chaudhary@users.noreply.github.com>
2024-06-18 12:31:22 +0530

    Add files via upload

---
+++ MultDelg.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiDelegate
+{
+    delegate void MDelegate();
+    internal class MultDelg
+    {
+        public static void Display()
+        {
+            Console.WriteLine("Banglore");
+        }
+        public static void Print()
+        {
+            Console.WriteLine("Delhi");
+        }
+    }
+}
