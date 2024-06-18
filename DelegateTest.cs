f6b071e0ece8d5e45aab62ce9ff6306b5af8cdce
Urmila-Chaudhary <161577372+Urmila-Chaudhary@users.noreply.github.com>
2024-06-18 12:30:28 +0530

    Add files via upload

---
+++ DelegateTest.cs
@@ -0,0 +1,20 @@
+﻿namespace delegates
+{
+    
+    internal class DelegateTest
+    {
+       
+
+        static void Main(string[] args)
+        {
+            ArithOp op1 = new ArithOp(MathOperation.Add);
+            ArithOp op2 = new ArithOp(MathOperation.Sub);
+
+            int result1 = op1(200, 100);
+            int result2 = op2(200, 100);
+            Console.WriteLine(" Sum = "+ result1);
+            Console.WriteLine(" Difference = " + result2);
+
+        }
+    }
+}
