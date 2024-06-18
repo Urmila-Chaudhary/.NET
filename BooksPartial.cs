d51210b575abcb9044c0fb18968bdabd3e96be58
Urmila-Chaudhary <161577372+Urmila-Chaudhary@users.noreply.github.com>
2024-06-18 12:32:02 +0530

    Add files via upload

---
+++ BooksPartial.cs
@@ -0,0 +1,13 @@
+﻿namespace PartialClass
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Books b1 = new Books("Pride & Prejuidice","Jane Austen");
+            b1.PrintDetails();
+            Books b2 = new Books(20, "Penguin House");
+            b2.PrintPubDetails();
+        }
+    }
+}
