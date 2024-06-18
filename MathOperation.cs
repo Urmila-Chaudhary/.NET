002cd970f6921295f1a56caa9b5350b7c402b610
Urmila-Chaudhary <161577372+Urmila-Chaudhary@users.noreply.github.com>
2024-06-18 12:30:41 +0530

    Add files via upload

---
+++ MathOperation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegates
+
+{
+    delegate int ArithOp(int x, int y);
+    internal class MathOperation
+    {
+        public static int Add(int a, int b)
+        {
+            return (a + b);
+        }
+
+        public static int Sub(int a, int b)
+        {
+            return (a - b);
+        }
+    }
+}
