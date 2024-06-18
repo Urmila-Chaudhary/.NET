507be71f57060d77425e9781acf6bb523460e32f
Urmila-Chaudhary <161577372+Urmila-Chaudhary@users.noreply.github.com>
2024-06-18 12:31:33 +0530

    Add files via upload

---
+++ MultiCastDelgates.cs
@@ -0,0 +1,19 @@
+﻿namespace MultiDelegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MDelegate m1= new MDelegate(MultDelg.Display);
+            MDelegate m2 = new MDelegate(MultDelg.Print);
+            MDelegate m3 = m1 + m2;
+            MDelegate m4 = m2 - m1;
+            MDelegate m5 = m3 - m2;
+
+            m3();
+            m4();
+            m5();
+
+        }
+    }
+}
