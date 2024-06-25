e43e632a2ed71ff9a7a2072244d843c7382554cf
Urmila-Chaudhary <161577372+Urmila-Chaudhary@users.noreply.github.com>
2024-06-25 12:18:37 +0530

    Add files via upload

---
+++ Generic_deligates.cs
@@ -0,0 +1,13 @@
+﻿namespace Generic_delegates
+{
+    internal class Program
+    {
+        public delegate int SumDeligate(int x, int y);
+
+        static void Main(string[] args)
+        {
+            SumDeligate sd1 = delegate(int x , int y) { return x + y; };
+            Console.WriteLine($"Upon calling the anonymous function using delegate, Sum: {sd1(18,2)}");
+        }
+    }
+}
